4e3eecb4ac8f406270ad6c22e0649aaef7bad5a2
Nathan Reed <reednj@gmail.com>
2010-08-06 17:44:10 +0800

    RDL Save: + uses standard connection dialog + initial load now async + downloading from relative directorys works as expected

--- Reporting Tools/RDLSave/RDPRip.Designer.cs
+++ Reporting Tools/RDLSave/RDPRip.Designer.cs
@@ -32,13 +32,15 @@ namespace RDLSave
             this.StartButton = new System.Windows.Forms.Button();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.LoadList_Worker = new System.ComponentModel.BackgroundWorker();
             this.ReportTreeList = new ReportingTools.Common.ReportTree();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // StartButton
             // 
-            this.StartButton.Location = new System.Drawing.Point(490, 473);
+            this.StartButton.Location = new System.Drawing.Point(490, 496);
             this.StartButton.Name = "StartButton";
             this.StartButton.Size = new System.Drawing.Size(153, 28);
             this.StartButton.TabIndex = 1;
@@ -62,20 +64,34 @@ namespace RDLSave
             this.StatusLabel.Size = new System.Drawing.Size(19, 17);
             this.StatusLabel.Text = "...";
             // 
+            // LoadList_Worker
+            // 
+            this.LoadList_Worker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.LoadList_Worker_DoWork);
+            this.LoadList_Worker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.LoadList_Worker_RunWorkerCompleted);
+            // 
             // ReportTreeList
             // 
             this.ReportTreeList.ImageIndex = 0;
-            this.ReportTreeList.Location = new System.Drawing.Point(12, 12);
+            this.ReportTreeList.Location = new System.Drawing.Point(12, 28);
             this.ReportTreeList.Name = "ReportTreeList";
             this.ReportTreeList.SelectedImageIndex = 0;
-            this.ReportTreeList.Size = new System.Drawing.Size(631, 455);
+            this.ReportTreeList.Size = new System.Drawing.Size(631, 462);
             this.ReportTreeList.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(655, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
             // 
             // RDPRip
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(655, 549);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.StartButton);
             this.Controls.Add(this.ReportTreeList);
@@ -84,6 +100,7 @@ namespace RDLSave
             this.Name = "RDPRip";
             this.Text = "Save Rdls";
             this.Load += new System.EventHandler(this.RDPRip_Load);
+            this.Activated += new System.EventHandler(this.RDPRip_Activated);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -97,6 +114,8 @@ namespace RDLSave
         private System.Windows.Forms.Button StartButton;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+        private System.ComponentModel.BackgroundWorker LoadList_Worker;
+        private System.Windows.Forms.ToolStrip toolStrip1;
     }
 }
 
--- Reporting Tools/RDLSave/RDPRip.cs
+++ Reporting Tools/RDLSave/RDPRip.cs
@@ -14,6 +14,10 @@ namespace RDLSave
 {
     public partial class RDPRip : Form
     {
+        bool hasStarted = false;
+        ServiceState CurrentState = ServiceState.Disconnected;
+        SSRSUri ServerUrl = null;
+
         ReportingService rs = new ReportingService();
         BackgroundWorker Download_Worker = new BackgroundWorker();
 
@@ -29,38 +33,89 @@ namespace RDLSave
 
         private void RDPRip_Load(object sender, EventArgs e)
         {
-            rs.Credentials = System.Net.CredentialCache.DefaultCredentials;
-            rs.Url = SSRSUri.ParseString("hydrogen/nate").ToUrl();
+            
 
-            CatalogItem[] serverItems = rs.ListChildren("/", true);
+        }
 
+        private void RDPRip_Activated(object sender, EventArgs e)
+        {
+            if (hasStarted == false)
+            {
+                hasStarted = true;
+                RunConnectDialog(true);
+            }
+        }
+
+        private void LoadList_Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            StatusLabel.Text = "Loading Reports...";
+            e.Result = rs.ListChildren("/", true);
+        }
+
+        private void LoadList_Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                throw e.Error;
+            }
+
+            CatalogItem[] serverItems = e.Result as CatalogItem[];
+            
             foreach (CatalogItem curItem in serverItems)
             {
                 ReportTreeList.AddReport(curItem);
             }
 
             ReportTreeList.Nodes[0].Expand();
+            StatusLabel.Text = "Connected to SERVER/INSATNACE";
         }
 
         private void StartButton_Click(object sender, EventArgs e)
         {
             StartButton.Enabled = false;
             StatusLabel.Text = "Starting Download...";
-            Download_Worker.RunWorkerAsync();
+            Download_Worker.RunWorkerAsync(new DownloadArgs("/", @"C:\Dev\Temp\"));
         }
 
         void Download_Worker_DoWork(object sender, DoWorkEventArgs e)
         {
-            string RootPath = @"C:\Dev\Temp\";
-            CatalogItem[] serverItems = rs.ListChildren("/", true);
+            DownloadArgs args = e.Argument as DownloadArgs;
+            string FromFolder = RemoveEndSlash(args.SourcePath);
+            string DestRootPath = args.DestPath + Path.GetFileName(FromFolder);
+
+            CatalogItem[] serverItems = rs.ListChildren(FromFolder, true);
+            Directory.CreateDirectory(DestRootPath);
 
             int i = 0;
             foreach (CatalogItem item in serverItems)
             {
                 i++;
-                DownloadReportItem(item, RootPath);
+
+                if (item.Type != ItemTypeEnum.Folder)
+                {
+                    Byte[] data = DownloadReportItem(item);
+
+                    if (data != null)
+                    {
+                        string TempFromFolder = AddEndSlash(FromFolder);
+                        string FilePath = item.Path.Remove(item.Path.IndexOf(TempFromFolder), TempFromFolder.Length);
+                        FilePath = Path.Combine(DestRootPath, FilePath);
+                        FilePath = (item.Type == ItemTypeEnum.Report) ? FilePath + ".rdl" : FilePath;
+                        File.WriteAllBytes(FilePath, data);
+                    }
+                }
+                else
+                {
+                    string TempFromFolder = AddEndSlash(FromFolder);
+                    string RelativePath = item.Path.Remove(item.Path.IndexOf(TempFromFolder), TempFromFolder.Length);
+                    Directory.CreateDirectory(Path.Combine(DestRootPath, RelativePath));
+                }
+
+                
                 Download_Worker.ReportProgress(i * 100 / serverItems.Length, item.Name);
             }
+
+            e.Result = serverItems.Length;
         }
 
         void Download_Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
@@ -70,36 +125,80 @@ namespace RDLSave
 
         void Download_Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            StatusLabel.Text = "All Items Downloaded";
+            if (e.Error != null)
+            {
+                throw e.Error;
+            }
+            
+            StatusLabel.Text = String.Format("{0} Items Downloaded", e.Result);
             StartButton.Enabled = true;
         }
 
-
-        private void DownloadReportItem(CatalogItem ReportItem, string RootPath)
+        private Byte[] DownloadReportItem(CatalogItem ReportItem)
         {
-            // we want to remove the initial '/' from the path, otherwise
-            // the Path.Combine method does not work correctly.
-            string ReletivePath = (ReportItem.Path[0] == '/') ? ReportItem.Path.Substring(1) : ReportItem.Path;
-
+           
             if (ReportItem.Type == ItemTypeEnum.Report)
             {
-                Byte[] data = rs.GetReportDefinition(ReportItem.Path);
-                string dest_file = Path.Combine(RootPath, ReletivePath + ".rdl");
-                File.WriteAllBytes(dest_file, data);
+                return rs.GetReportDefinition(ReportItem.Path);
+
             }
             else if (ReportItem.Type == ItemTypeEnum.Resource)
             {
                 string MimeType;
-                Byte[] data = rs.GetResourceContents(ReportItem.Path, out MimeType);
-                string dest_file = Path.Combine(RootPath, ReletivePath);
-                File.WriteAllBytes(dest_file, data);
+                return rs.GetResourceContents(ReportItem.Path, out MimeType);
             }
-            else if (ReportItem.Type == ItemTypeEnum.Folder)
+
+            return null;
+        }
+
+ 
+        private string RemoveEndSlash(string FilePath)
+        {
+            // we want to remove the initial '/' from the path, otherwise
+            // the Path.Combine method does not work correctly.
+            FilePath = FilePath.Trim();
+            return (FilePath[FilePath.Length - 1] == '/' && FilePath.Length > 1) ? FilePath.Substring(0, FilePath.Length - 1) : FilePath;
+        }
+
+        private string AddEndSlash(string FilePath)
+        {
+            FilePath = FilePath.Trim();
+            return (FilePath[FilePath.Length - 1] != '/') ? FilePath + "/" : FilePath;
+        }
+
+        private void RunConnectDialog(bool AutoLogin)
+        {
+            LoginForm lf = new LoginForm(AutoLogin);
+            if (lf.ShowDialog() == DialogResult.OK)
             {
-                Directory.CreateDirectory(Path.Combine(RootPath, ReletivePath));
+                // disconnect. TODO: a general method to set the ui on a state change.
+                this.CurrentState = ServiceState.Disconnected;
+
+                // if the user click ok on the connection dialog box, set the rs url
+                // and start getting the data. Otherwise to nothing...
+                this.ServerUrl = lf.ServerUrl;
+                rs.Url = this.ServerUrl.ToUrl();
+                rs.Credentials = lf.RsCredentials;
+                LoadList_Worker.RunWorkerAsync();
+
             }
         }
 
 
+
+        
     }
+
+    public class DownloadArgs
+    {
+        public string SourcePath;
+        public string DestPath;
+
+        public DownloadArgs(string SourcePath, string DestPath)
+        {
+            this.SourcePath = SourcePath.Trim();
+            this.DestPath = DestPath.Trim();
+        }
+    }
+
 }
